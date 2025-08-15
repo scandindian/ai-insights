11571d50108274dd098a725805871afc5d910e47
Varun Kumar <heysadboy@gmail.com>
2025-08-16 00:05:41 +0200

    Update app general style
    Improve app style and theme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import NavigationBar from './components/NavigationBar';
 import Home from './pages/Home';
 import Users from './pages/Users';
 import Departments from './pages/Departments';
 
+const AppContainer = styled.div`
+    min-height: 100vh;
+    background: #f5f6fa;
+`;
+
 const App: React.FC = () => (
-    <BrowserRouter>
-        <NavigationBar />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/departments" element={<Departments />} />
-        </Routes>
-    </BrowserRouter>
+    <AppContainer>
+        <BrowserRouter>
+            <NavigationBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/departments" element={<Departments />} />
+            </Routes>
+        </BrowserRouter>
+    </AppContainer>
 );
 
 export default App;
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const NavBar = styled.nav`
-    background: #222;
-    padding: 0.5rem 2rem;
+    background: #fff;
+    padding: 0.75rem 2rem;
     display: flex;
     align-items: center;
+    box-shadow: 0 2px 8px rgba(60, 64, 67, 0.07);
+    width: 100%;
+    max-width: 100vw;
 `;
 
 const NavList = styled.ul`
@@ -20,14 +23,17 @@ const NavList = styled.ul`
 const NavItem = styled.li``;
 
 const NavLink = styled(Link)`
-    color: #fff;
+    color: #1976d2;
     text-decoration: none;
     font-weight: 500;
     font-size: 1rem;
-    transition: color 0.2s;
+    padding: 0.5rem 1rem;
+    border-radius: 4px;
+    transition: background 0.2s, color 0.2s;
 
     &:hover {
-        color: #61dafb;
+        background: #e3f2fd;
+        color: #1565c0;
     }
 `;
 
